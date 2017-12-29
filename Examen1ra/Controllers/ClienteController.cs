9827c34f7b96763882e200892f88ffac886760a6
Zeroexe00 <exe_6084@hotmail.com>
2017-12-29 20:28:09 -0300

    listo

---
+++ Examen1ra/Controllers/ClienteController.cs
@@ -0,0 +1,53 @@
+﻿using Examen1ra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen1ra.Controllers
+{
+    public class ClienteController : Controller
+    {
+        Examen1ra.Models.TiendaEntities cnx;
+
+        public ClienteController()
+        {
+
+            cnx = new Models.TiendaEntities();
+        }
+        public ActionResult Listado()
+        {
+
+            return View(cnx.Cliente.ToList());
+        }
+
+        public ActionResult Mantenedor()
+        {
+
+            return View();
+        }
+        public ActionResult Guardar(int rut, string nombre, string apellido, string direccion, string tipocliente)
+        {
+            Cliente nuevo = new Cliente()
+            {
+                rut = rut,
+                nombre = nombre,
+                apellido = apellido,
+                direccion = direccion,
+                tipocliente = tipocliente
+            };
+            cnx.Cliente.Add(nuevo);
+            cnx.SaveChanges();
+
+            return View("Listado", cnx.Cliente.ToList());
+        }
+
+        public ActionResult Ficha(int rut)
+        {
+
+            return View(cnx.Cliente.Where(x => x.rut == rut).First());
+        }
+        
+    }
+}
---
+++ Examen1ra/Controllers/ProductosController.cs
@@ -0,0 +1,55 @@
+﻿using Examen1ra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen1ra.Controllers
+{
+    public class ProductosController : Controller
+    {
+        Examen1ra.Models.TiendaEntities cnx;
+    
+    public ProductosController() {
+            cnx = new Models.TiendaEntities();
+
+        }
+
+
+        public ActionResult Listado()
+    {
+
+        return View(cnx.Productos.ToList());
+    }
+
+    public ActionResult Mantenedor()
+    {
+
+        return View();
+    }
+    public ActionResult Guardar(int codigobarra, string nombre, string familia, int precio, int descuentomax, string descripcion)
+    {
+        Productos nuevo = new Productos()
+        {
+            codigobarra = codigobarra,
+            nombre = nombre,
+            familia = familia,
+            precio = precio,
+            descuentomax = descuentomax,
+            descripcion=descripcion
+        };
+        cnx.Productos.Add(nuevo);
+        cnx.SaveChanges();
+
+        return View("Listado", cnx.Productos.ToList());
+    }
+
+    public ActionResult Ficha(int codigobarra)
+    {
+
+        return View(cnx.Productos.Where(x => x.codigobarra == codigobarra).First());
+    }
+
+}
+}
